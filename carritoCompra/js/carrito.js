7fa7672ce54e8ea16ef09320bb8cc252ce778204
Maricarmen <lopezromeromaricarmen03gmail.com>
2022-06-07 00:38:22 -0500

    Respaldo26

--- carritoCompra/js/carrito.js
+++ carritoCompra/js/carrito.js
@@ -1,80 +1,91 @@
-const carrito = document.getElementById("carrito");
-const listaProductos= document.getElementById("lista_productos");
-const contenedorCarrito= document.getElementById("lista_carrito");
-const vaciarCarritoBtn= document.getElementById("vaciar_carrito");
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaProductos = document.querySelector('#lista_productos');
+const contenedorCarrito = document.querySelector('#lista_carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar_carrito'); 
+let articulosCarrito = [];
 
-let articuloCarrito =[];
-
+// Listeners
 cargarEventListeners();
 
 function cargarEventListeners() {
-    listaProductos.addEventListener('click',agregarProducto);
-    carrito.addEventListener('click', eliminarProducto);
-    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+     listaProductos.addEventListener('click', agregarProducto);
+     carrito.addEventListener('click', eliminarProducto);
+     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
 }
 
 function agregarProducto(e) {
     e.preventDefault();
-    if (e.target.classList.contains('agregarCarrito')) {
-        const producto=e.target.parentElement.parentElement;
-        leerDatosProductos(producto);
+    if(e.target.classList.contains('agregarCarrito')) {
+         const producto = e.target.parentElement.parentElement;
+         leerDatosProducto(producto);
     }
 }
 
-function leerDatosProductos(producto) {
-    const infoProducto ={
-        imagen: producto.getElementById("imagen").src,
-        titulo: producto.getElementById("h4").textContent,
-        precio: producto.getElementById(".precio span").textContent,
-        id: producto.getElementById("a").getAttribute("data-id"),
-        cantidad: 1
+function leerDatosProducto(producto) {
+    const infoProducto = {
+         imagen: producto.querySelector('img').src,
+         titulo: producto.querySelector('h4').textContent,
+         precio: producto.querySelector('div p span').textContent,
+         id: producto.querySelector('a').getAttribute('data-id'), 
+         cantidad: 1
     }
 
-    if (articuloCarrito.some(producto => producto.id === infoProducto.id)) {
-        const productos = articuloCarrito.map(producto => {
-            if (producto.id === infoProducto.id) {
-                producto.cantidad++;
-                return producto;
-            } else {
-                return producto;
+    if( articulosCarrito.some( producto => producto.id === infoProducto.id ) ) { 
+         const productos = articulosCarrito.map( producto => {
+              if( producto.id === infoProducto.id ) {
+                   producto.cantidad++;
+                    return producto;
+               } else {
+                    return producto;
             }
-        })
-        articuloCarrito = [...productos];
-    } else {
-        articuloCarrito =[...articuloCarrito, infoProducto];
+         });
+         articulosCarrito = [...productos];
+    }  else {
+         articulosCarrito = [...articulosCarrito, infoProducto];
     }
+    carritoHTML();
 }
+
 
 function eliminarProducto(e) {
     e.preventDefault();
-    if (e.target.classList.contains('borrarProducto')) {
-        const productoId = e.target.getAttribute('data-id')
-        articuloCarrito = articuloCarrito.filter(producto => producto.id !== productoId);
-        carritoHTML();
+    if(e.target.classList.contains('borrar_producto') ) {
+         const productoId = e.target.getAttribute('data-id')
+         articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
+         carritoHTML();
     }
 }
+
 
 function carritoHTML() {
+
     vaciarCarrito();
-    articuloCarrito.forEach(producto =>{
-        const row=document.createElement('tr');
-        row.innerHTML = `
-            <td>
-                <img src="${producto.imagen}" width=100>
-            </td>
-            <td>${producto.titulo}</td>
-            <td>${producto.precio}</td>
-            <td>${producto.cantidad}</td>
-            <td>
-                <a href="#" class="borrarProducto" data-id="${producto.id}">X</a>
-            </td>
-        `;
-        contenedorCarrito.appendChild(row);    
+
+    articulosCarrito.forEach(producto => {
+         const row = document.createElement('tr');
+         row.innerHTML = `
+              <td>  
+                   <img src="${producto.imagen}" width=100>
+              </td>
+              <td>${producto.titulo}</td>
+              <td>${producto.precio}</td>
+              <td>${producto.cantidad} </td>
+              <td>
+                   <a href="#" class="borrar_producto" data-id="${producto.id}">X</a>
+              </td>
+         `;
+         contenedorCarrito.appendChild(row);
+         actualizarPrecio()
     });
+
 }
 
-function  vaciarCarrito() {
-    while (contenedorCarrito.firstChild) {
-        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
-    }
+
+
+function vaciarCarrito() {
+    while(contenedorCarrito.firstChild) {
+         contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+     }
 }
